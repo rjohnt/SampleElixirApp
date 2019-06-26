a267ecdf08ac2ad1a0f300b6b98711f30eb3f234
Ryan Timmons <ryanjohntimmons@gmail.com>
2019-06-25 21:45:29 -0500

    Adds several method plugs.

--- lib/hello_web/controllers/message_controller.ex
+++ lib/hello_web/controllers/message_controller.ex
@@ -1,12 +1,42 @@
 defmodule HelloWeb.MessageController do
   use HelloWeb, :controller
 
-  plug :put_headers, %{content_encoding: "gzip", cache_control: "max-age=3600"}
-  plug :put_layout, "bare.html"
+  plug :authenticate
+  plug :fetch_message
+  plug :authorize_message
 
-  def put_headers(conn, key_values) do
-    Enum.reduce key_values, conn, fn {k, v}, conn ->
-      Plug.Conn.put_resp_header(conn, to_string(k), v)
+  def show(conn, params) do
+    render(conn, :show, page: find_message(params["id"]))
+  end
+
+  defp authenticate(conn, _) do
+      case Authenticator.find_user(conn) do
+        {:ok, user} ->
+          assign(conn, :user, user)
+        :error ->
+          conn |> put_flash(:info, "You must be logged in.") |> redirect(to: "/") |> halt()
+      end
+  end
+
+  defp fetch_message(conn, _) do
+    case find_message(conn.params["id"]) do
+      nil ->
+        conn |> put_flash(:info, "That message wasn't found.") |> redirect(to: "/") |> halt()
+      message ->
+        assign(conn, :message, message)
     end
+  end
+
+  defp authorize_message(conn, _) do
+    if Authorizer.can_access?(conn.assigns[:user], conn.assigns[:message]) do
+      conn
+    else
+      conn |> put_flash(:info, "You can't access that page.") |> redirect(to: "/") |> halt()
+    end
+  end
+
+  # Mock method.
+  defp find_message(id) do
+    if String.length(id) |> rem(2) == 0, do: "message", else: nil
   end
 end
